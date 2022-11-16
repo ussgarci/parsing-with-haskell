7404c9c4ff4a0b6936813caed71b598a57df9485
sgarcia <stevensantosgarcia@gmail.com>
2022-11-16 06:52:58 -0600

    Add test suite

---
+++ src/Basic.hs
@@ -0,0 +1,7 @@
+module Basic where
+
+-- First try running the test suites with this function as "undefined".
+-- Then fill in the proper function definition and see that the tests pass!
+-- simpleMathFunction a b c = a * b - c
+simpleMathFunction :: Int -> Int -> Int -> Int
+simpleMathFunction a b c = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,19 @@
+module Main where
+
+-- Run this test with the command:
+-- stack build Testing:test:unit-test
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Basic
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain simpleMathTests
+
+-- TODO: Add more cases!
+simpleMathTests :: TestTree
+simpleMathTests = testGroup "Simple Math Tests"
+  [ testCase "Small Numbers" $
+      simpleMathFunction 3 4 5 @?= 7
+  ]
